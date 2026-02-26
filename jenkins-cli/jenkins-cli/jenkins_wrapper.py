6d0353eba33662d88495b9eaf6efa382b380eea5
Sumith-DevOps <mysumith@gmail.com>
2026-02-25 22:14:09 -0500

    Update jenkins_wrapper.py

--- jenkins-cli/jenkins-cli/jenkins_wrapper.py
+++ jenkins-cli/jenkins-cli/jenkins_wrapper.py
@@ -1,7 +1,7 @@
 import jenkins
 class JenkinsWrapper:
     def getJobs(self):
-        jenkins_server = jenkins.Jenkins('http://mdc2vr6133:8080/')
+        jenkins_server = jenkins.Jenkins('http://jenkins:8080/')
         print(jenkins_server.jobs_count())
         my_job = jenkins_server.get_job_config('Chef_Deploy')
         print(my_job)
